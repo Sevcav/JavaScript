12403ad332138e0eb6ef23081fd1fc1e2629d256
Ken <kchapman2@cocc.edu>
2017-10-05 08:28:46 -0700

    Testing Button and Using a Function To Launch Game

--- Scripts/battleship.js
+++ Scripts/battleship.js
@@ -1,7 +1,10 @@
 /* CIS-135JS Ken Chapman 
 BattlelShip Game Chapter 2 of HeadStart JS
 Modified with LET Function instead of VAR
+Converting to a Function
+
 */
+function BBGAME() {
 
 // Random Creates a number between .01 to .99, Floor Rounds Dwn
 LET randomLoc = Math.floor(Math.random() * 5); 
@@ -40,3 +43,4 @@ LET stats = "You took " + guesses + " guesses to sink the battleship, " +
             "which means your shooting accuracy was " + (3/guesses);
 
 alert(stats);
+}
