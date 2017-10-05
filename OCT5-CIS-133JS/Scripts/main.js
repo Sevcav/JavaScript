757e450dc323c527af05c06c0e11fb2183bf1d9e
KLC <32318793+Sevcav@users.noreply.github.com>
2017-10-05 12:41:05 -0700

    OCT 5 Class updates
    Using Text Box

--- OCT5-CIS-133JS/Scripts/main.js
+++ OCT5-CIS-133JS/Scripts/main.js
@@ -3,6 +3,34 @@ function test() {
     alert("hello world");
 }
 
+function copyInputToOutput() {
+    let inputText = getElementText("inputText");
+    /*
+    let inputElement = document.getElementById("inputText");
+    let inputText = inputElement.value; two step full method */
+    alert(inputText);
+    let OutputElement = document.getElementById("outputText");
+    OutputElement.value = inputText;
+}
+
+// Convienence function to get the HTML element
+// Pass in element ID as a string
+function getE(id) {
+    return document.getElementById(id);
+}
+
+// Return the value of a HTML element 
+// Pass in the element ID as a string 
+function getElementText(id) {
+    return getE(id).value;
+}
+
+// Simple test to check the Element ID functions 
+function test() {
+    alert();
+}
+
+
 function readText() {
     //Grab the input element from the document
     let inputItem = document.getElementById("stuff");
