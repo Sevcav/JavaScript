75780595ea0ac111ffe5d1c7a1aca5e814517181
Ken <kchapman2@cocc.edu>
2017-10-12 17:21:27 -0700

    Updated with Array check

--- Scripts/battleship.js
+++ Scripts/battleship.js
@@ -1,9 +1,23 @@
 /* CIS-133JS Ken Chapman 
 BattlelShip Game Chapter 2 of HeadStart JS
 Modified with let Function instead of VAR
-Converting to a Function
+Converted to using Funtions
+Added an array and a function to check if passed parameter is within array
+
 
 */
+
+//returns true if passed param is in array, or false if it does not 
+function includes(k) {
+	for(var i=0; i < this.length; i++){
+	  if( this[i] === k || ( this[i] !== this[i] && k !== k ) ){
+		return true;
+	  }
+	}
+	return false;
+  }
+
+
 function BBGAME() {
 
 // Random Creates a number between .01 to .99, Floor Rounds Dwn
@@ -12,6 +26,7 @@ let location1 = randomLoc;
 let location2 = location1 + 1;
 let location3 = location1 + 2;
 let guess;
+let prevguess = []; //Build an Array of previous Guesses
 let hits = 0;
 let guesses = 0;
 let isSunk = false;
@@ -23,9 +38,13 @@ while (isSunk == false) {
 		alert("Please enter a valid cell number!");
 	} else{
 
-		// Might place a subIF here to check if A previous HIT location is Re-entered
-		
-        guesses = guesses + 1;
+		//added a check here to see if guess has already been made
+	if (prevguess.includes(guess)) {
+		alert("That cell has already been guessed before!")
+	} else{
+			
+		++guesses; //increment guesses by 1
+		prevguess.push (guess); //add guess to prevguess array
         
 		if (guess == location1 || guess == location2 || guess == location3) {
 			alert("HIT!");
@@ -39,7 +58,7 @@ while (isSunk == false) {
 			alert("MISS");
 		}
 	}
-}
+}}
 
 let stats = "You took " + guesses + " guesses to sink the battleship, " +
             "which means your shooting accuracy was " + (3/guesses);
