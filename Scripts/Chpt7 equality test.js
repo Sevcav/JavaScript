be0f30439c447b5a92e9b653d7c6670a3f3acd6f
Ken <kchapman2@cocc.edu>
2017-10-16 08:45:08 -0700

    added comments fro understanding what is taking place

--- Scripts/Chpt7 equality test.js
+++ Scripts/Chpt7 equality test.js
@@ -1,11 +1,13 @@
+// this loop returns in what position of the array lot is the car located 
 function findCarINLot(car) {
     for (var i = 0; i < lot.length; i++) {
         if (car === lot[i]) {
             return i;
         }
     }
-    retrun -1;
+    retrun - 1;
 }
+//These desciptions Make and Model make no impact to the findCarINLot function call
 var chevy = {
     make: "Chevy",
     model: "Bel Air"
@@ -23,9 +25,11 @@ var fiat2 = {
     model: "500"
 };
 
+//chevy = pos 0, taxi = pos 1, fiat1 = pos 2, fiat2 = pos 3 
 var lot = [chevy, taxi, fiat1, fiat2];
 
-var loc1 = findCarINLot (fiat2);
-var loc2 = findCarINLot (taxi);
-var loc3 = findCarINLot (chevy);
-var loc4 = findCarINLot (fiat1);
+// Call the finCarInlot function and step through and determine which position in the array the car is in
+var loc1 = findCarINLot(fiat2); //3
+var loc2 = findCarINLot(taxi); //1
+var loc3 = findCarINLot(chevy); //0
+var loc4 = findCarINLot(fiat1); //2
